efff7826a311cec4baf1a21d0a3a9c3f93c9848c
Celal ORMANLI <ormanlicelal@gmail.com>
2020-05-04 23:35:24 +0300

    add timer for dice and cpin

--- app/src/main/java/com/qijik/randomcreator/Dice.kt
+++ app/src/main/java/com/qijik/randomcreator/Dice.kt
@@ -1,16 +1,23 @@
 package com.qijik.randomcreator
 
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import java.util.concurrent.ThreadLocalRandom
 import com.r0adkll.slidr.Slidr
+import kotlinx.android.synthetic.main.activity_dice.*
 import java.sql.Time
+import java.util.*
+import kotlin.concurrent.scheduleAtFixedRate
 
 class Dice : AppCompatActivity() {
 
@@ -25,47 +32,72 @@ class Dice : AppCompatActivity() {
         diceImage.setImageResource(R.drawable.dice_1)
         diceImage1.setImageResource(R.drawable.dice_1)
         Slidr.attach(this)
+        val timer = Timer("schedule", true);
         toDice.setOnClickListener {
-                var coin = ThreadLocalRandom.current().nextInt(1, 7)
-                if (coin == 1) {
-                    diceImage.setImageResource(R.drawable.dice_1)
+            for(x in 1..25) {
+                if (x<20) {
+                    val handler = Handler(Looper.myLooper())
+                    handler.postDelayed({
+                        toDiceFun()
+                    }, 200 * x.toLong())
+                }else{
+                    val handler = Handler(Looper.myLooper())
+                    handler.postDelayed({
+                        if(x%2==0) {
+                            diceImage.visibility = View.INVISIBLE
+                            diceImage1.visibility=View.INVISIBLE
+                        }
+                        else{
+                            diceImage.visibility = View.VISIBLE
+                            diceImage1.visibility=View.VISIBLE
+                        }
+                    }, 250 * x.toLong())
                 }
-                if (coin == 2) {
-                    diceImage.setImageResource(R.drawable.dice_2)
                 }
-                if (coin == 3) {
-                    diceImage.setImageResource(R.drawable.dice_3)
-                }
-                if (coin == 4) {
-                    diceImage.setImageResource(R.drawable.dice_4)
-                }
-                if (coin == 5) {
-                    diceImage.setImageResource(R.drawable.dice_5)
-                }
-                if (coin == 6) {
-                    diceImage.setImageResource(R.drawable.dice_6)
-                }
-                var coin1 = ThreadLocalRandom.current().nextInt(1, 7)
-                if (coin1 == 1) {
-                    diceImage1.setImageResource(R.drawable.dice_1)
-                }
-                if (coin1 == 2) {
-                    diceImage1.setImageResource(R.drawable.dice_2)
-                }
-                if (coin1 == 3) {
-                    diceImage1.setImageResource(R.drawable.dice_3)
-                }
-                if (coin1 == 4) {
-                    diceImage1.setImageResource(R.drawable.dice_4)
-                }
-                if (coin1 == 5) {
-                    diceImage1.setImageResource(R.drawable.dice_5)
-                }
-                if (coin1 == 6) {
-                    diceImage1.setImageResource(R.drawable.dice_6)
-                }
+
+
             }
         }
+    fun toDiceFun(){
+        var coin = ThreadLocalRandom.current().nextInt(1, 7)
+        if (coin == 1) {
+            diceImage.setImageResource(R.drawable.dice_1)
+        }
+        if (coin == 2) {
+            diceImage.setImageResource(R.drawable.dice_2)
+        }
+        if (coin == 3) {
+            diceImage.setImageResource(R.drawable.dice_3)
+        }
+        if (coin == 4) {
+            diceImage.setImageResource(R.drawable.dice_4)
+        }
+        if (coin == 5) {
+            diceImage.setImageResource(R.drawable.dice_5)
+        }
+        if (coin == 6) {
+            diceImage.setImageResource(R.drawable.dice_6)
+        }
+        var coin1 = ThreadLocalRandom.current().nextInt(1, 7)
+        if (coin1 == 1) {
+            diceImage1.setImageResource(R.drawable.dice_1)
+        }
+        if (coin1 == 2) {
+            diceImage1.setImageResource(R.drawable.dice_2)
+        }
+        if (coin1 == 3) {
+            diceImage1.setImageResource(R.drawable.dice_3)
+        }
+        if (coin1 == 4) {
+            diceImage1.setImageResource(R.drawable.dice_4)
+        }
+        if (coin1 == 5) {
+            diceImage1.setImageResource(R.drawable.dice_5)
+        }
+        if (coin1 == 6) {
+            diceImage1.setImageResource(R.drawable.dice_6)
+        }
+    }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         onBackPressed()
         return super.onOptionsItemSelected(item)
--- app/src/main/java/com/qijik/randomcreator/HeadsOrTails.kt
+++ app/src/main/java/com/qijik/randomcreator/HeadsOrTails.kt
@@ -3,6 +3,8 @@ package com.qijik.randomcreator
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
@@ -21,17 +23,41 @@ class HeadsOrTails : AppCompatActivity() {
         Slidr.attach(this)
         var flipCoin:Button=findViewById(R.id.flipCoin)
         var tailsOrHeadsImage:ImageView=findViewById(R.id.headsOrTailsImage)
-
         flipCoin.setOnClickListener {
-            var coin = ThreadLocalRandom.current().nextInt(0,1000)
-            if (coin%2==0) {
-
-                tailsOrHeadsImage.setImageResource(R.drawable.head)
+            for(x in 1..25) {
+                if (x<20) {
+                    val handler = Handler(Looper.myLooper())
+                    handler.postDelayed({
+                        flipFun()
+                    }, 200 * x.toLong())
+                }else{
+                    val handler = Handler(Looper.myLooper())
+                    handler.postDelayed({
+                        if(x%2==0) {
+                            tailsOrHeadsImage.visibility = View.INVISIBLE
+                            tailsOrHeadsImage.visibility=View.INVISIBLE
+                        }
+                        else{
+                            tailsOrHeadsImage.visibility = View.VISIBLE
+                            tailsOrHeadsImage.visibility=View.VISIBLE
+                        }
+                    }, 250 * x.toLong())
+                }
             }
-            else {
 
-                tailsOrHeadsImage.setImageResource(R.drawable.tail)
-            }
+
+        }
+    }
+    fun flipFun(){
+        var tailsOrHeadsImage:ImageView=findViewById(R.id.headsOrTailsImage)
+        var coin = ThreadLocalRandom.current().nextInt(0,1000)
+        if (coin%2==0) {
+
+            tailsOrHeadsImage.setImageResource(R.drawable.head)
+        }
+        else {
+
+            tailsOrHeadsImage.setImageResource(R.drawable.tail)
         }
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
